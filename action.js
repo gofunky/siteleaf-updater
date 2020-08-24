c9ac0f7753b133c0d5eaa9305d261aa521a84480
Matthias Fax <matthias.fax@gmail.com>
2020-08-24 19:04:47 +0200

    fix: add all exit conditions for cli (#115)
    * test: temporarily disable put
    
    * fix: add all exit conditions for cli
    
    * [CodeFactor] Apply fixes
    
    [ci skip] [skip ci]
    
    * chore(dist): update the distributed action script
    
    * fix: publish action input being falsely parsed as string
    
    * chore(dist): update the distributed action script
    
    Co-authored-by: codefactor-io <support@codefactor.io>
    Co-authored-by: matfax <matfax@users.noreply.github.com>

--- action.js
+++ action.js
@@ -3,14 +3,16 @@
 const core = require('@actions/core')
 const lib = require('./lib')
 
+const publish = String(core.getInput('publish')).toLowerCase() === 'true'
+
 lib(core.getInput('api-key'), core.getInput('api-secret'), core.getInput('site'),
-  core.getInput('page'), core.getInput('file'), core.getInput('publish'))
+  core.getInput('page'), core.getInput('file'), publish)
   .then(name => {
     core.setOutput('name', name)
-    if (core.getInput('publish')) {
-      console.info(`Page "${name}" was successfully updated and published`)
+    if (publish) {
+      core.info(`Page "${name}" was successfully updated and published`)
     } else {
-      console.info(`Page "${name}" was successfully updated`)
+      core.info(`Page "${name}" was successfully updated`)
     }
   })
   .catch(core.setFailed)
